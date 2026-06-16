50b0374bf82de024299ff6df0081107c7b0ed33e
Clive106 <shikaliclive@gmail.com>
2026-06-16 10:57:14 +0300

    branching by use of multi functions and case statement

---
+++ lib/extract_user.ex
@@ -0,0 +1,25 @@
+defmodule ElixirDemo.ExtractUser do
+  def extract_user(user)do
+    case extract_login(user)do
+      {:error, reason} -> {:error, reason}
+      {:ok, login} ->
+        case extract_email(user)do
+          {:error, reason} -> {:error, reason}
+          {:ok, email} ->
+            case extract_password(user)do
+              {:error, reason} -> {:error, reason}
+              {:ok, password} -> {:ok, %{"login" => login, "email" => email, "password" => password}}
+            end
+          end
+        end
+  end
+
+  defp extract_login(%{"login" => login}), do: {:ok, login}
+  defp extract_login(_), do: {:error, "Missing"}
+
+  defp extract_email(%{"email" => email}), do: {:ok, email}
+  defp extract_email(_), do: {:error, "Missing email"}
+
+  defp extract_password(%{"password" => password}), do: {:ok, password}
+  defp extract_password(_), do: {:error, "Missing password"}
+end
