b3819c274f7c2cc73e454f8e6fd8ce9453af58d3
Clive106 <shikaliclive@gmail.com>
2026-09-07 14:37:46 +0300

    update:  added key value store to server process

--- lib/elixir_demo/server_process.ex
+++ lib/elixir_demo/server_process.ex
@@ -1,10 +1,11 @@
 defmodule ServerProcess do
-  def start(callback_module)do
+  def start(callback_module) do
     spawn(fn ->
       initial_state = callback_module.init()
       loop(callback_module, initial_state)
     end)
   end
+
   defp loop(callback_module, current_state) do
     receive do
       {request, caller} ->
@@ -15,11 +16,45 @@ defmodule ServerProcess do
     end
   end
 
-  def call(server_pid, request)do
+  def call(server_pid, request) do
     send(server_pid, {request, self()})
 
     receive do
       {:response, response} -> response
     end
+  end
+end
+
+defmodule KeyValueStore do
+  def init() do
+    %{}
+  end
+
+  def handle_call({:put, key, value}, state) do
+    {:ok, Map.put(state, key, value)}
+  end
+
+  def handle_call({:get, key}, state) do
+    {Map.get(state, key), state}
+  end
+
+  def handle_call({:delete, key}, state) do
+    {:ok, Map.delete(state, key)}
+  end
+
+  def start() do
+    ServerProcess.start(KeyValueStore)
+  end
+
+  def put(pid, key, value) do
+    ServerProcess.call(pid, {:put, key, value})
+  end
+
+  def get(pid, key) do
+    ServerProcess.call(pid, {:get, key})
+  end
+
+  def delete(pid, key) do
+    ServerProcess.call(pid, {:delete, key})
   end
 end
