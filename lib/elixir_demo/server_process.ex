13792b28b55533ba24a5e4ac21511a107e215966
Clive106 <shikaliclive@gmail.com>
2026-09-07 15:19:39 +0300

    added cast requests

--- lib/elixir_demo/server_process.ex
+++ lib/elixir_demo/server_process.ex
@@ -8,20 +8,29 @@ defmodule ServerProcess do
 
   defp loop(callback_module, current_state) do
     receive do
-      {request, caller} ->
+      {:call, request, caller} ->
         {response, new_state} = callback_module.handle_call(request, current_state)
         send(caller, {:response, response})
+
+        loop(callback_module, new_state)
+
+      {:cast, request} ->
+        new_state = callback_module.handle_cast(request, current_state)
 
         loop(callback_module, new_state)
     end
   end
 
   def call(server_pid, request) do
-    send(server_pid, {request, self()})
+    send(server_pid, {:call, request, self()})
 
     receive do
       {:response, response} -> response
     end
+  end
+
+  def cast(server_pid, request) do
+    send(server_pid, {:cast, request})
   end
 end
 
@@ -30,16 +39,20 @@ defmodule KeyValueStore do
     %{}
   end
 
-  def handle_call({:put, key, value}, state) do
-    {:ok, Map.put(state, key, value)}
-  end
-
   def handle_call({:get, key}, state) do
     {Map.get(state, key), state}
   end
 
-  def handle_call({:delete, key}, state) do
-    {:ok, Map.delete(state, key)}
+  def handle_cast({:put, key, value}, state) do
+    Map.put(state, key, value)
+  end
+
+  # def handle_call({:delete, key}, state) do
+  #   {:ok, Map.delete(state, key)}
+  # end
+
+  def handle_cast({:delete, key}, state) do
+    Map.delete(state, key)
   end
 
   def start() do
@@ -47,7 +60,7 @@ defmodule KeyValueStore do
   end
 
   def put(pid, key, value) do
-    ServerProcess.call(pid, {:put, key, value})
+    ServerProcess.cast(pid, {:put, key, value})
   end
 
   def get(pid, key) do
@@ -55,6 +68,6 @@ defmodule KeyValueStore do
   end
 
   def delete(pid, key) do
-    ServerProcess.call(pid, {:delete, key})
+    ServerProcess.cast(pid, {:delete, key})
   end
 end
